901f0ecbfa718c4c2132851f1b0b28850206bc7b
Angel <jose.prieto@levva.io>
2023-02-25 14:25:06 -0300

    init project

---
+++ src/user/dto/edit-user.dto.ts
@@ -0,0 +1,18 @@
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class EditUserDto {
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+}
